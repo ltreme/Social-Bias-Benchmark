5995318147eedb16c0e6901ca8fafebf70e86e92
Markus Ertl <markus@MacBookPro.fritz.box>
2025-06-10 14:10:13 +0200

    update benchmark.sh for new main.py

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-accelerate launch app/main.py
+accelerate launch app/main.py --model_name "meta-llama/Llama-3.3-70B-Instruct" --benchmark_type "bias" --mixed_precision "fp16"
 
 echo "✅ Benchmark completed"
