14548969dd51dbcf88b14703f02a91698568b951
Markus Ertl <mertl@hm.edu>
2025-06-11 16:17:16 +0200

    add mistral

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 accelerate launch app/main.py --model_name "meta-llama/Llama-3.3-70B-Instruct" --benchmark_type "bias" --mixed_precision "fp16"
+accelerate launch app/main.py --model_name "mistralai/Mistral-Small-24B-Instruct-2501" --benchmark_type "bias" --mixed_precision "fp16"
 
 echo "✅ Benchmark completed"
