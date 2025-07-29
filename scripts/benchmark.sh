21e2608735c9af58e9384fa67fe8ba038beb72e3
Markus Ertl <mertl@hm.edu>
2025-07-29 10:12:30 +0200

    update benchmark script for persona generation

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-accelerate launch app/main.py --model_name "meta-llama/Llama-3.3-70B-Instruct" --benchmark_type "bias" --mixed_precision "fp16"
-accelerate launch app/main.py --model_name "mistralai/Mistral-Small-24B-Instruct-2501" --benchmark_type "bias" --mixed_precision "fp16"
+python apps/benchmark/src/benchmark/cli/run_preprocessing.py --model_name mistralai/Mistral-Small-24B-Instruct-2501
 
 echo "✅ Benchmark completed"
